12f1b6d0729f8b91064729542f0631e56134d622
stonebird <1245863260@qq.com>
2025-03-02 20:45:14 +0800

    feat:semi design

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,48 @@
 import './App.css';
+import "reset-css/reset.css";
+import {
+  Nav,
+  Avatar,
+  Dropdown,
+} from '@douyinfe/semi-ui';
+
+import {
+  IconEmoji,
+  IconUser,
+  IconFemale,
+  IconLanguage,
+  IconUserCardVideo,
+  IconComment,
+  IconMoon,
+  IconGithubLogo,
+} from '@douyinfe/semi-icons';
+
 
 const App = () => {
   return (
-    <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
+    <div style={{ width: '100%' }}>
+      <Nav
+        mode={'horizontal'}
+        items={[
+          { itemKey: 'user', text: '个人简介', icon: <IconUser /> },
+          { itemKey: 'ocr', text: '图文识别', icon: <IconLanguage /> },
+          { itemKey: 'media', text: '媒体', icon: <IconUserCardVideo /> },
+          { itemKey: 'chart', text: 'AI对话', icon: <IconComment /> },
+        ]}
+        header={{
+          logo: <IconEmoji style={{ height: '36px', fontSize: 36 }} />,
+          text: 'GalaxyScope'
+        }}
+        footer={
+          <>
+          <IconMoon style={{ margin: 4, marginBottom: 4 }} size="extra-large" />
+              <IconGithubLogo style={{ margin: 4 }} size="extra-large" />
+              <Avatar size="small" color='light-blue' style={{ margin: 4 }}>胡静</Avatar>
+              <span>lisan530</span>
+              <IconFemale />
+          </>
+        }
+      />
     </div>
   );
 };
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router";
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<App />} />
+        <Route path="settings" element={<App />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
